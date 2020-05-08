1befbcbcd25c4f755cccf90069ad11adc9f19548
Васькович Виталий <vvv93rus@gmail.com>
2020-05-08 16:27:05 +0300

    Недостающие файлы и готовая компиляция install
    Недостающие файлы и готовая компиляция install

---
+++ V8AddInLibrary/AddIn/Stub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using TCX.CallTriggerCmd;
+using V8.AddIn;
+
+[ComVisible(true)]
+[Guid("adbd4ca6-6ad7-4d6f-9dfc-d6d2f07c0dab")] // произвольный Guid-идентификатор Вашей компоненты
+[ProgId("AddIn.SIPConnector")] // это имя COM-объекта, по которому Вы будете ее подключать
+public class Stub : LanguageExtenderAddIn
+{
+    public Stub() : base(typeof(SIPConnector), 1000) { }
+}
+   
